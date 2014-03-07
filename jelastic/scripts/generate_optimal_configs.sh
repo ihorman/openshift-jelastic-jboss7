1a55bc990b60fab8f6c6c18298172e1bd363738c
Ihor Kolodyuk <ik@jelastic.com>
2014-03-07 17:32:50 +0200

    fixes

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -3,16 +3,8 @@
 SED=$(which sed);
 
 #
-# config optimizer for jetty8
+# config optimizer for jboss7
 #
 
-JETTY_START_SCRIPT="${OPENSHIFT_JETTY8_DIR}.openshift/action_hooks/start";
-
-[ -z "$XMS" ] && { XMS=32; }
-memory_total=`free -m | grep Mem | awk '{print $2}'`;
-[ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}";  }
-
-$SED -i  "s/-Xms[0-9]*m/-Xms${XMS}m/g"  $JETTY_START_SCRIPT;
-$SED -i  "s/-Xmx[0-9]*m/-Xmx${XMX}m/g"  $JETTY_START_SCRIPT;
 
 
