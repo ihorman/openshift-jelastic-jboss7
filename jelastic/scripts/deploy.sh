0958352974f273928ebf05ea3a252366ed4adc36
ihorman <ihorman@gmail.com>
2014-03-20 18:46:32 +0200

    Update deploy.sh

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -6,14 +6,12 @@ WGET=$(which wget);
 
 function _deploy(){
      [ "x${context}" == "xroot" ] && context="ROOT";
-     [ -f "${WEBROOT}/${context}.war" ] &&  rm -f "${WEBROOT}/${context}.war";
-     [ -f "${WEBROOT}/${context}.war" ] &&  rm -f "${WEBROOT}/${context}.war.*";
+     [ -f "${WEBROOT}/${context}.war" ] && { rm -f "${WEBROOT}/${context}.war"; rm -f "${WEBROOT}/${context}.war.*" ; };
      $WGET --no-check-certificate --content-disposition -O "${WEBROOT}/${context}.war" "$package_url";
 }
 
 function _undeploy(){
      [ "x${context}" == "xroot" ] && context="ROOT";
-     [ -f "${WEBROOT}/${context}.war" ] && rm -f "${WEBROOT}/${context}.war";
-     [ -f "${WEBROOT}/${context}.war" ] &&  rm -f "${WEBROOT}/${context}.war.*";
+     [ -f "${WEBROOT}/${context}.war" ] &&  { rm -f "${WEBROOT}/${context}.war"; rm -f "${WEBROOT}/${context}.war.*" ; };
 }
 
