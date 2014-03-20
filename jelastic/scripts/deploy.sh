fa3a1a4530cb77f5b8729a1b43d1178e991f8275
Ihor Kolodyuk <ik@jelastic.com>
2014-03-20 17:04:08 +0200

    JBoss

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -5,13 +5,15 @@
 WGET=$(which wget);
 
 function _deploy(){
-     [ "x${context}" == "xROOT" ] && context="root";
+     [ "x${context}" == "xroot" ] && context="ROOT";
      [ -f "${WEBROOT}/${context}.war" ] &&  rm -f "${WEBROOT}/${context}.war";
+     [ -f "${WEBROOT}/${context}.war" ] &&  rm -f "${WEBROOT}/${context}.war.*";
      $WGET --no-check-certificate --content-disposition -O "${WEBROOT}/${context}.war" "$package_url";
 }
 
 function _undeploy(){
-     [ "x${context}" == "xROOT" ] && context="root";
+     [ "x${context}" == "xroot" ] && context="ROOT";
      [ -f "${WEBROOT}/${context}.war" ] && rm -f "${WEBROOT}/${context}.war";
+     [ -f "${WEBROOT}/${context}.war" ] &&  rm -f "${WEBROOT}/${context}.war.*";
 }
 
